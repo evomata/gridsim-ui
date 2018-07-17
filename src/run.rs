3c68404e7f433553e4945bf1dbebb869b32008f8
Geordon Worley <vadixidav@gmail.com>
2018-07-17 19:30:42 -0400

    remove all flamegraph code

--- src/run.rs
+++ src/run.rs
@@ -1,17 +1,12 @@
-#[cfg(feature = "flame_it")]
-use flame;
 use glium::{
     self, glutin::{self, WindowEvent},
 };
 use gridsim::{GetNeighbors, Sim, SquareGrid, TakeMoveNeighbors};
-#[cfg(feature = "flame_it")]
-use std::fs::File;
 use Renderer;
 
 const SCALED_DEFAULT: f32 = 10.0;
 
 /// Runs a grid with default window setup. Draws true as white and false as black.
-#[cfg_attr(feature = "flame_it", flame)]
 pub fn basic_bool<'a, S: 'a>(grid: SquareGrid<'a, S>)
 where
     S: Sim<'a, Cell = bool>,
@@ -27,7 +22,6 @@ where
 }
 
 // Runs a grid with each cell in the window is scale pixels^2. Draws true as white and false as black.
-#[cfg_attr(feature = "flame_it", flame)]
 pub fn scaled_bool<'a, S: 'a>(grid: SquareGrid<'a, S>, scale: f32)
 where
     S: Sim<'a, Cell = bool>,
@@ -43,7 +37,6 @@ where
 }
 
 /// Runs a grid with default window setup and a coloration function.
-#[cfg_attr(feature = "flame_it", flame)]
 pub fn basic<'a, S: 'a, F>(grid: SquareGrid<'a, S>, coloration: F)
 where
     S: Sim<'a>,
@@ -60,7 +53,6 @@ where
 }
 
 /// Runs a grid with each cell in the window is scale pixels^2 and a coloration function.
-#[cfg_attr(feature = "flame_it", flame)]
 pub fn scaled<'a, S: 'a, F>(grid: SquareGrid<'a, S>, coloration: F, scale: f32)
 where
     S: Sim<'a>,
@@ -77,7 +69,6 @@ where
 }
 
 /// Runs a grid with default window setup, a coloration function, and a filter for which cells to draw.
-#[cfg_attr(feature = "flame_it", flame)]
 pub fn basic_filter<'a, S: 'a, Color, Filter>(
     grid: SquareGrid<'a, S>,
     coloration: Color,
@@ -98,7 +89,6 @@ pub fn basic_filter<'a, S: 'a, Color, Filter>(
 }
 
 /// Runs a grid with each cell in the window is scale pixels^2, a coloration function, and a filter for which cells to draw.
-#[cfg_attr(feature = "flame_it", flame)]
 pub fn scaled_filter<'a, S: 'a, Color, Filter>(
     mut grid: SquareGrid<'a, S>,
     coloration: Color,
@@ -127,7 +117,6 @@ pub fn scaled_filter<'a, S: 'a, Color, Filter>(
 
     loop {
         use glium::Surface;
-        #[cfg_attr(feature = "flame_it", flame)]
         grid.cycle();
 
         let mut target = display.draw();
@@ -153,12 +142,6 @@ pub fn scaled_filter<'a, S: 'a, Color, Filter>(
                     match event {
                         // Break from the main loop when the window is closed.
                         WindowEvent::Closed => {
-                            // Dump the report to disk
-                            #[cfg(feature = "flame_it")]
-                            flame::dump_html(&mut File::create("flame-graph.html").unwrap())
-                                .expect(
-                                    "gridsim-ui::run::basic_filter(): unable to write flamegraph",
-                                );
                             finish = true;
                         }
                         _ => (),
